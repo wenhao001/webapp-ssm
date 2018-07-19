cbe80d6f18d3ec3a1640f8dbfb8cc10fd4aa1950
wenhao001 <liwenhao051@163.com>
2018-07-20 00:26:31 +0800

    区分页面接口和数据接口

--- src/main/java/com/wenhao/ssh/controller/IndexController.java
+++ src/main/java/com/wenhao/ssh/controller/IndexController.java
@@ -1,23 +1,35 @@
 package com.wenhao.ssh.controller;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
     private static final Logger LOGGER = LoggerFactory.getLogger(IndexController.class);
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-//    @ResponseBody
-    public String index() {
-        LOGGER.info("index");
+    public String home() {
         return "index";
+    }
+
+    @RequestMapping(value = "/page/{path}", method = RequestMethod.GET)
+    public String index(@PathVariable(value = "path")  String path) {
+        LOGGER.info("path:{}", path);
+        return StringUtils.isNotBlank(path) ? path : "index";
+    }
+
+
+    @RequestMapping(value = "/data/{path}", method = RequestMethod.GET)
+    @ResponseBody
+    public String json(@PathVariable(value = "path")  String path) {
+        LOGGER.info("path:{}", path);
+        return "{\"a\":1}";
     }
 
 }
